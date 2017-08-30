5be5163fdb77bde4014dba79fafd43acb3e5ff3e
Pedro Almeida <jpedro89@gmail.com>
2017-08-30 16:34:17 +0100

    [Menu] Create Element Factory

---
+++ app/page/view/panel/utils/ElementFactory.js
@@ -0,0 +1,30 @@
+function createElementWithId(elementType, elementId) {
+    var element = document.createElement(elementType);
+    element.id = elementId;
+    return element;
+}
+
+function createElementWithIdAndClassName(elementType, elementId, elementClassName) {
+    var element = createElementWithId(elementType, elementId)
+    element.className = elementClassName;
+    return element;
+}
+
+function createElementWithIdAndTextNode(elementType, elementId, elementTextNode) {
+    var element = createElementWithId(elementType, elementId);
+    element.appendChild(document.createTextNode(elementTextNode));
+    return element;
+}
+
+function createElementWithIdClassNameAndTextNode(elementType, elementId, elementClassName, elementTextNode) {
+    var element = createElementWithIdAndClassName(elementType, elementId, elementClassName);
+    element.appendChild(document.createTextNode(elementTextNode));
+    return element;
+}
+
+module.exports = {
+    createElementWithId: createElementWithId,
+    createElementWithIdAndClassName: createElementWithIdAndClassName,
+    createElementWithIdAndTextNode: createElementWithIdAndTextNode,
+    createElementWithIdClassNameAndTextNode: createElementWithIdClassNameAndTextNode
+};
---
+++ test/unit/page/view/panel/utils/ElementFactoryTest.js
@@ -0,0 +1,38 @@
+var jsdom = require('mocha-jsdom');
+var expect = require('chai').expect;
+var ElementFactory = require('../../../../../../app/page/view/panel/utils/ElementFactory');
+
+describe('Element Factory', function () {
+
+    jsdom();
+
+    it('should create element with id', function () {
+        var element = ElementFactory.createElementWithId('div', 'anElementId');
+        expect(element.outerHTML).to.equal('<div id="anElementId"></div>');
+    });
+
+    it('should create element with id and single class name', function () {
+        var element = ElementFactory.createElementWithIdAndClassName('div', 'anElementId', 'anElementClassName');
+        expect(element.outerHTML).to.equal('<div id="anElementId" class="anElementClassName"></div>');
+    });
+
+    it('should create element with id and multiple class names', function () {
+        var element = ElementFactory.createElementWithIdAndClassName('button', 'anElementId', 'anElementClassName aSecondElementClassName');
+        expect(element.outerHTML).to.equal('<button id="anElementId" class="anElementClassName aSecondElementClassName"></button>');
+    });
+
+    it('should create element with id and text node', function () {
+        var element = ElementFactory.createElementWithIdAndTextNode('div', 'anElementId', 'Text Node Content');
+        expect(element.outerHTML).to.equal('<div id="anElementId">Text Node Content</div>');
+    });
+
+    it('should create element with id, single class name and text node', function () {
+        var element = ElementFactory.createElementWithIdClassNameAndTextNode('button', 'anElementId', 'anElementClassName', 'Text Node Content');
+        expect(element.outerHTML).to.equal('<button id="anElementId" class="anElementClassName">Text Node Content</button>');
+    });
+
+    it('should create element with id, multiple class names and text node', function () {
+        var element = ElementFactory.createElementWithIdClassNameAndTextNode('div', 'anElementId', 'anElementClassName aSecondElementClassName', 'Text Node Content');
+        expect(element.outerHTML).to.equal('<div id="anElementId" class="anElementClassName aSecondElementClassName">Text Node Content</div>');
+    });
+});

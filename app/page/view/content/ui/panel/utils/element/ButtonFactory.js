d51a37db4e1f3e136eebf22914e67142d6a92dbe
Pedro Almeida <jpedro89@gmail.com>
2017-09-11 21:48:49 +0100

    [Action Panel] Add callback buttons

--- app/page/view/content/ui/panel/utils/element/ButtonFactory.js
+++ app/page/view/content/ui/panel/utils/element/ButtonFactory.js
@@ -1,67 +1,69 @@
 var ElementFactory = require('./ElementFactory');
 
-function createButtonWithClassNameAndText(buttonId, buttonClassName, buttonText) {
+function createButtonWithClassNameAndText(buttonId, buttonClassName, buttonText, buttonOnClickCallBack) {
     var button = ElementFactory.createElementWithIdClassNameAndTextNode('button', buttonId, buttonClassName, buttonText);
     button.setAttribute('type', 'button');
+    button.addEventListener('click', buttonOnClickCallBack);
     return button;
 }
 
-function createButtonWithClassNameGlyphIconAndText(buttonId, buttonClassName, glyphIcon, buttonText) {
+function createButtonWithClassNameGlyphIconAndText(buttonId, buttonClassName, glyphIcon, buttonText, buttonOnClickCallBack) {
     var button = ElementFactory.createElementWithIdAndClassName('button', buttonId, buttonClassName);
     button.setAttribute('type', 'button');
     button.appendChild(glyphIcon);
-    if (buttonText) {
+    if (buttonText !== '') {
         button.appendChild(document.createTextNode(' ' + buttonText));
     }
+    button.addEventListener('click', buttonOnClickCallBack);
     return button;
 }
 
-function createButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-default', buttonText);
+function createButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-default', buttonText, buttonOnClickCallBack);
 }
 
-function createPrimaryButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-primary', buttonText);
+function createPrimaryButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-primary', buttonText, buttonOnClickCallBack);
 }
 
-function createSuccessButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-success', buttonText);
+function createSuccessButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-success', buttonText, buttonOnClickCallBack);
 }
 
-function createInfoButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-info', buttonText);
+function createInfoButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-info', buttonText, buttonOnClickCallBack);
 }
 
-function createWarningButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-warning', buttonText);
+function createWarningButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-warning', buttonText, buttonOnClickCallBack);
 }
 
-function createDangerButton(buttonId, buttonText) {
-    return createButtonWithClassNameAndText(buttonId, 'btn btn-danger', buttonText);
+function createDangerButton(buttonId, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameAndText(buttonId, 'btn btn-danger', buttonText, buttonOnClickCallBack);
 }
 
-function createButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-default', glyphIcon, buttonText);
+function createButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-default', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
-function createPrimaryButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-primary', glyphIcon, buttonText);
+function createPrimaryButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-primary', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
-function createSuccessButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-success', glyphIcon, buttonText);
+function createSuccessButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-success', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
-function createInfoButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-info', glyphIcon, buttonText);
+function createInfoButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-info', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
-function createWarningButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-warning', glyphIcon, buttonText);
+function createWarningButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-warning', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
-function createDangerButtonWithGlyphIcon(buttonId, glyphIcon, buttonText) {
-    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-danger', glyphIcon, buttonText);
+function createDangerButtonWithGlyphIcon(buttonId, glyphIcon, buttonText, buttonOnClickCallBack) {
+    return createButtonWithClassNameGlyphIconAndText(buttonId, 'btn btn-danger', glyphIcon, buttonText, buttonOnClickCallBack);
 }
 
 module.exports = {
--- test/unit/page/view/content/ui/panel/utils/element/ButtonFactoryTest.js
+++ test/unit/page/view/content/ui/panel/utils/element/ButtonFactoryTest.js
@@ -7,93 +7,147 @@ describe('Button Factory', function () {
 
     jsdom();
 
-    it('should create button', function () {
-        var button = ButtonFactory.createButton('button-id', 'Back');
+    it('should create button', function (done) {
+        var button = ButtonFactory.createButton('button-id', 'Back', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-default" type="button">Back</button>');
+        button.click();
     });
 
-    it('should create primary button', function () {
-        var button = ButtonFactory.createPrimaryButton('button-id', 'Search');
+    it('should create primary button', function (done) {
+        var button = ButtonFactory.createPrimaryButton('button-id', 'Search', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-primary" type="button">Search</button>');
+        button.click();
     });
 
-    it('should create success button', function () {
-        var button = ButtonFactory.createSuccessButton('button-id', 'Save');
+    it('should create success button', function (done) {
+        var button = ButtonFactory.createSuccessButton('button-id', 'Save', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-success" type="button">Save</button>');
+        button.click();
     });
 
-    it('should create info button', function () {
-        var button = ButtonFactory.createInfoButton('button-id', 'Bookmark');
+    it('should create info button', function (done) {
+        var button = ButtonFactory.createInfoButton('button-id', 'Bookmark', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-info" type="button">Bookmark</button>');
+        button.click();
     });
 
-    it('should create warning button', function () {
-        var button = ButtonFactory.createWarningButton('button-id', 'Flag');
+    it('should create warning button', function (done) {
+        var button = ButtonFactory.createWarningButton('button-id', 'Flag', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-warning" type="button">Flag</button>');
+        button.click();
     });
 
-    it('should create danger button', function () {
-        var button = ButtonFactory.createDangerButton('button-id', 'Delete');
+    it('should create danger button', function (done) {
+        var button = ButtonFactory.createDangerButton('button-id', 'Delete', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-danger" type="button">Delete</button>');
+        button.click();
     });
 
-    it('should create button with glyph icon', function () {
-        var button = ButtonFactory.createButtonWithGlyphIcon('button-id', GlyphIconFactory.ARROW_LEFT(), 'Back');
+    it('should create button with glyph icon', function (done) {
+        var button = ButtonFactory.createButtonWithGlyphIcon('button-id', GlyphIconFactory.ARROW_LEFT(), 'Back', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-default" type="button"><span class="glyphicon glyphicon-arrow-left"></span> Back</button>');
+        button.click();
     });
 
-    it('should create primary button with glyph icon', function () {
-        var button = ButtonFactory.createPrimaryButtonWithGlyphIcon('button-id', GlyphIconFactory.SEARCH(), 'Search');
+    it('should create primary button with glyph icon', function (done) {
+        var button = ButtonFactory.createPrimaryButtonWithGlyphIcon('button-id', GlyphIconFactory.SEARCH(), 'Search', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-primary" type="button"><span class="glyphicon glyphicon-search"></span> Search</button>');
+        button.click();
     });
 
-    it('should create success button with glyph icon', function () {
-        var button = ButtonFactory.createSuccessButtonWithGlyphIcon('button-id', GlyphIconFactory.SAVE_FILE(), 'Save');
+    it('should create success button with glyph icon', function (done) {
+        var button = ButtonFactory.createSuccessButtonWithGlyphIcon('button-id', GlyphIconFactory.SAVE_FILE(), 'Save', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-success" type="button"><span class="glyphicon glyphicon-save-file"></span> Save</button>');
+        button.click();
     });
 
-    it('should create info button with glyph icon', function () {
-        var button = ButtonFactory.createInfoButtonWithGlyphIcon('button-id', GlyphIconFactory.PAPERCLIP(), 'Bookmark');
+    it('should create info button with glyph icon', function (done) {
+        var button = ButtonFactory.createInfoButtonWithGlyphIcon('button-id', GlyphIconFactory.PAPERCLIP(), 'Bookmark', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-info" type="button"><span class="glyphicon glyphicon-paperclip"></span> Bookmark</button>');
+        button.click();
     });
 
-    it('should create warning button with glyph icon', function () {
-        var button = ButtonFactory.createWarningButtonWithGlyphIcon('button-id', GlyphIconFactory.FLAG(), 'Flag');
+    it('should create warning button with glyph icon', function (done) {
+        var button = ButtonFactory.createWarningButtonWithGlyphIcon('button-id', GlyphIconFactory.FLAG(), 'Flag', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-warning" type="button"><span class="glyphicon glyphicon-flag"></span> Flag</button>');
+        button.click();
     });
 
-    it('should create danger button with glyph icon', function () {
-        var button = ButtonFactory.createDangerButtonWithGlyphIcon('button-id', GlyphIconFactory.REMOVE(), 'Delete');
+    it('should create danger button with glyph icon', function (done) {
+        var button = ButtonFactory.createDangerButtonWithGlyphIcon('button-id', GlyphIconFactory.REMOVE(), 'Delete', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-danger" type="button"><span class="glyphicon glyphicon-remove"></span> Delete</button>');
+        button.click();
     });
 
-    it('should create button with glyph icon and without text', function () {
-        var button = ButtonFactory.createButtonWithGlyphIcon('button-id', GlyphIconFactory.ARROW_LEFT());
+    it('should create button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createButtonWithGlyphIcon('button-id', GlyphIconFactory.ARROW_LEFT(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-default" type="button"><span class="glyphicon glyphicon-arrow-left"></span></button>');
+        button.click();
     });
 
-    it('should create primary button with glyph icon and without text', function () {
-        var button = ButtonFactory.createPrimaryButtonWithGlyphIcon('button-id', GlyphIconFactory.SEARCH());
+    it('should create primary button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createPrimaryButtonWithGlyphIcon('button-id', GlyphIconFactory.SEARCH(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-primary" type="button"><span class="glyphicon glyphicon-search"></span></button>');
+        button.click();
     });
 
-    it('should create success button with glyph icon and without text', function () {
-        var button = ButtonFactory.createSuccessButtonWithGlyphIcon('button-id', GlyphIconFactory.SAVE_FILE());
+    it('should create success button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createSuccessButtonWithGlyphIcon('button-id', GlyphIconFactory.SAVE_FILE(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-success" type="button"><span class="glyphicon glyphicon-save-file"></span></button>');
+        button.click();
     });
 
-    it('should create info button with glyph icon and without text', function () {
-        var button = ButtonFactory.createInfoButtonWithGlyphIcon('button-id', GlyphIconFactory.PAPERCLIP());
+    it('should create info button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createInfoButtonWithGlyphIcon('button-id', GlyphIconFactory.PAPERCLIP(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-info" type="button"><span class="glyphicon glyphicon-paperclip"></span></button>');
+        button.click();
     });
 
-    it('should create warning button with glyph icon and without text', function () {
-        var button = ButtonFactory.createWarningButtonWithGlyphIcon('button-id', GlyphIconFactory.FLAG());
+    it('should create warning button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createWarningButtonWithGlyphIcon('button-id', GlyphIconFactory.FLAG(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-warning" type="button"><span class="glyphicon glyphicon-flag"></span></button>');
+        button.click();
     });
 
-    it('should create danger button with glyph icon and without text', function () {
-        var button = ButtonFactory.createDangerButtonWithGlyphIcon('button-id', GlyphIconFactory.REMOVE());
+    it('should create danger button with glyph icon and without text', function (done) {
+        var button = ButtonFactory.createDangerButtonWithGlyphIcon('button-id', GlyphIconFactory.REMOVE(), '', function () {
+            done();
+        });
         expect(button.outerHTML).to.equal('<button id="button-id" class="btn btn-danger" type="button"><span class="glyphicon glyphicon-remove"></span></button>');
+        button.click();
     });
 });
